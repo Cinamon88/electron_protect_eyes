f93b9fe77028e01b48c297626528a532fc52ca8e
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-01-26 15:49:51 +0100

    add startTimer

--- app/script.js
+++ app/script.js
@@ -9,28 +9,36 @@ const App = () => {
 
   let seconds = String(Math.floor(time % 60)).padStart(2, '0');
   let minutes = String(Math.floor(time / 60)).padStart(2, '0');
+
+  const startTimer = () => {
+    setTime(1200);
+    setStatus('work');
+    setTimer(setInterval(() => {
+      setTime(time => time + 1);
+    }));
+  }
   
-    return (
-      <div>
-        <h1>Protect your eyes</h1>
-        { status === 'off' && (
-          <div>
-            <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-            <p>This app will help you track your time and inform you when it's time to rest.</p>
+  return (
+    <div>
+      <h1>Protect your eyes</h1>
+      { status === 'off' && (
+        <div>
+          <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+          <p>This app will help you track your time and inform you when it's time to rest.</p>
+        </div>
+      )}
+      { status === 'work' && (<img src="./images/work.png" />)}
+      { status === 'rest' && (<img src="./images/rest.png" />)}
+      { status !== 'off' && (
+        <div className="timer">
+          {minutes}:{seconds}
           </div>
-        )}
-        { status === 'work' && (<img src="./images/work.png" />)}
-        { status === 'rest' && (<img src="./images/rest.png" />)}
-        { status !== 'off' && (
-          <div className="timer">
-            18:23
-          </div>
-        )}
-        { status === 'off' && (<button className="btn">Start</button>)}
-        { status !== 'off' && (<button className="btn">Stop</button>)}
-        <button className="btn btn-close">X</button>
-      </div>
-    );
+      )}
+      { status === 'off' && (<button className="btn" onClick={startTimer}>Start</button>)}
+      { status !== 'off' && (<button className="btn">Stop</button>)}
+      <button className="btn btn-close">X</button>
+    </div>
+  );
   
 };
 
